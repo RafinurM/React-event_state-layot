3825051d925b3095561bacea86885bd4eb608332
Rafinur Mursalimov <RMursalimov2@slb.com>
2022-12-15 12:18:36 +0700

    add key

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -4,7 +4,7 @@ import React from "react";
 function ShopItem(data) {
     return (
         <div className="ShopItem">
-            <div className="ShopItemImageWrapper"><img className="ShopItemImage" src={data.data.img}/></div>
+            <div className="ShopItemImageWrapper"><img className="ShopItemImage" src={data.data.img} alt={data.data.name}/></div>
             <div className="ShopItemName"><span>{data.data.name}</span></div>
             <div className="ShopItemColor"><span>{data.data.color}</span></div>
             <div className="ShopItemPrice"><span>${data.data.price}</span></div>
@@ -14,7 +14,7 @@ function ShopItem(data) {
 }
 
 function ListView(props) {
-    let products = props.items.map(element => <ShopItem data={element}/> )
+    let products = props.items.map((element, index) => <ShopItem key = {index} data={element}/> )
     
     return (
         <div className="ShopListWrapper">{products}</div>
